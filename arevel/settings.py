61f4e6b5bfabbbd783fb89efe5e648022de59a7b
Feni Varughese <feni727@gmail.com>
2018-02-07 21:32:29 -0600

    Added static and templates dirs

--- arevel/settings.py
+++ arevel/settings.py
@@ -49,7 +49,9 @@ ROOT_URLCONF = 'arevel.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [
+            os.path.join(BASE_DIR, "templates")
+        ],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
@@ -60,6 +62,10 @@ TEMPLATES = [
             ],
         },
     },
+]
+
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR, "static"),
 ]
 
 WSGI_APPLICATION = 'arevel.wsgi.application'
